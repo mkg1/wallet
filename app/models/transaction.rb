c94824b34ac99fccbc41dc5e3d984c605a9ec687
Molly G <gehring.mg@gmail.com>
2016-03-02 16:40:13 -0500

    Add Dashboard controller to display balance and number of transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,9 +3,9 @@ class Transaction < ActiveRecord::Base
 
   def self.balance
     balance = self.all.reduce(0.0) {|t, q| t + q.amount.to_f }
-
-    # array.reduce:+
-    # array.reduce(0) {|t, q| t + q}
   end
 
+  def self.total_transactions
+    self.count
+  end
 end
